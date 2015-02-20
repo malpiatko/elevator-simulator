702ed76485e571469a8a5ff16ae4eaec9179dc40
ruslanb <rulick@gmail.com>
2015-02-19 18:02:55 -0800

    Added requestFloor() method.

--- src/main/java/com/locusenergy/homework/Elevator.java
+++ src/main/java/com/locusenergy/homework/Elevator.java
@@ -1,6 +1,26 @@
 
 public interface Elevator {
+    /**
+     * Requests the Elevator to move to a certain floor. This method immitates press of a button inside the
+     * elevator. Therefore, it should not move the Elevator immidiately but just register the request.
+     * 
+     * Bonus: this method should throw InvalidStateException if the Elevator is NOT busy.
+     */
+    void requestFloor(int floor);
+    
+    /**
+     * Moves the Elevator to a desired floor.
+     */
     void moveElevator(int toFloor);
+    
+    /**
+     * Returns the internal state of the Elevator.
+     */
     boolean isBusy();
+    
+    /**
+     * Returns the floor where the Elevator is now.
+     */
     int currentFloor();
+    
 }
