0594962b5a155d1221e2437b14c9c762d9d42959
ruslanb <rulick@gmail.com>
2015-02-27 10:32:51 -0800

    removed moveElevator() / added package declaration

--- src/main/java/com/locusenergy/homework/Elevator.java
+++ src/main/java/com/locusenergy/homework/Elevator.java
@@ -1,17 +1,13 @@
+package com.locusenergy.homework;
 
 public interface Elevator {
     /**
-     * Requests the Elevator to move to a certain floor. This method immitates press of a button inside the
+     * Requests the Elevator to move to a certain floor. This method immitates pressing a button inside the
      * elevator. Therefore, it should not move the Elevator immidiately but just register the request.
      * 
      * Bonus: this method should throw InvalidStateException if the Elevator is NOT busy.
      */
     void requestFloor(int floor);
-    
-    /**
-     * Moves the Elevator to a desired floor.
-     */
-    void moveElevator(int toFloor);
     
     /**
      * Returns the internal state of the Elevator.
