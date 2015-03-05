4095c5f0af4f0ffd2936b10575cf901a7a1b6b92
ruslanb <rulick@gmail.com>
2015-03-04 17:45:56 -0800

    Renamed the Exception.

--- src/main/java/com/locusenergy/homework/ElevatorController.java
+++ src/main/java/com/locusenergy/homework/ElevatorController.java
@@ -8,7 +8,7 @@ public interface ElevatorController {
      * @param direction If > 0, going up; if <= 0 going down.
      * 
      * @return an instance of an Elevator
-     * @throws InvalidRequest when fromFloor < minimum floor, or > maximum floor, or direction is invalid.
+     * @throws InvalidRequestException when fromFloor < minimum floor, or > maximum floor, or direction is invalid.
      */
-    Elevator callElevator(int fromFloor, int direction) throws InvalidRequest;
+    Elevator callElevator(int fromFloor, int direction) throws InvalidRequestException;
 }
