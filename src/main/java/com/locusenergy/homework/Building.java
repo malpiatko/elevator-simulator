b1a6068f36144882e4aa92c2a8807419ab0aa092
ela <elzbieta.siwy@gmail.com>
2015-05-28 23:56:45 +0100

    A first mockup of elevator.

--- src/main/java/com/locusenergy/homework/Building.java
+++ src/main/java/com/locusenergy/homework/Building.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * Note: nFloors means that the last floor of the building is (nFloors - 1)
+ * Note: nFloors means that the last floor of the building is nFloors,
+ * therefore there are actually (nFloors + 1) with ground floor
  */
 public class Building implements ElevatorController{
 	
 	List<Elevator> elevators;
-	final int nFloors;
+	final int minFloor = 0;
+	final int maxFloor;
 	final int nElev;
 	
 	Building(int nElev, int nFloors) {
-		this.nFloors = nFloors;
+		this.maxFloor = nFloors;
 		this.nElev = nElev;
 		createElevators();
 	}
@@ -21,15 +23,19 @@ public class Building implements ElevatorController{
 	@Override
 	public Elevator callElevator(int fromFloor, int direction)
 			throws InvalidRequestException {
+		if (fromFloor > maxFloor || fromFloor < minFloor)
+			throw new InvalidRequestException();
+		if (fromFloor == maxFloor && direction > 0
+				|| fromFloor == minFloor && direction <=0)
+			throw new InvalidRequestException();
 		return null;
 	}
 	
 	private void createElevators() {
 		this.elevators = new ArrayList<Elevator>();
 		for(int i = 0; i <= nElev; i++) {
-			elevators.add(new ElevatorImp(i+1, nFloors));
+			elevators.add(new ElevatorImp(i+1, maxFloor));
 		}
 	}
-	
 	
 }
--- src/main/java/com/locusenergy/homework/ElevatorImp.java
+++ src/main/java/com/locusenergy/homework/ElevatorImp.java
@@ -1,35 +1,115 @@
 package com.locusenergy.homework;
 
-public class ElevatorImp implements Elevator {
-	
-	private final int id;
-	private final int nFloors;
-	private int currentFloor;
+import java.util.NavigableSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 
-	/*
+public class ElevatorImp implements Elevator, Runnable {
+	
+	private static final int FLOOR_TIME = 100;
+	private static final int OPEN_DOOR = 500;
+	
+	private static final int MOVING_UP = 1;
+	private static final int MOVING_DOWN = -1;
+	private static final int IDLE = 0;
+	private final int id;
+	private final int maxFloor;
+	private final int minFloor = 0;
+	
+	private int currentFloor;
+	private int currentDirection;
+	private boolean switchedOn;
+	private boolean busy;
+	Thread elevatorThread;
+	private NavigableSet<Integer> calls = new ConcurrentSkipListSet<Integer>();
+
+	/**
 	 * All lifts are created at the ground floor.
 	 * Each elevator has a unique (per Controller) id
 	 */
 	public ElevatorImp(int id, int nFloors) {
 		this.id = id;
-		this.nFloors = nFloors;
+		this.maxFloor = nFloors;
 		this.currentFloor = 0;
+		this.currentDirection = IDLE;
+		this.busy = false;
 	}
 
 	@Override
-	public void requestFloor(int floor) {
-		// TODO Auto-generated method stub
-
+	public synchronized void requestFloor(int floor) {
+		calls.add(floor);
 	}
 
 	@Override
 	public boolean isBusy() {
-		return false;
+		return (currentDirection == IDLE);
 	}
 
 	@Override
 	public int currentFloor() {
-		return 0;
+		return currentFloor;
+	}
+
+	@Override
+	public void run() {
+		while(switchedOn) {
+			if(currentDirection != 0) {
+				move(currentDirection);
+				checkArrived();
+				checkDirections();
+			} else {
+				sleep(FLOOR_TIME);
+			}
+				
+		}
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void switchOn() {
+		if(elevatorThread == null) {
+			elevatorThread = new Thread();
+		}
+		switchedOn = true;
+		elevatorThread.start();
+	}
+	
+	public void switchOff() {
+		switchedOn = false;
+	}
+	
+	private void move(int direction) {
+		currentFloor += direction;
+		sleep(FLOOR_TIME);
+	}
+	
+	private void checkArrived() {
+		if(calls.remove(currentFloor)) {
+			sleep(OPEN_DOOR);
+			if(calls.isEmpty()) {
+				currentDirection = IDLE;
+			}
+		}
+	}
+	
+	private void checkDirections() {
+		if(currentDirection == MOVING_UP) {
+			if(calls.higher(currentFloor) == null) {
+				currentDirection = MOVING_DOWN;
+			}
+		} else {
+			if(calls.lower(currentFloor) == null) {
+				currentDirection = MOVING_UP;
+			}
+		}
+	}
+	
+	private void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+		}	
 	}
 
 }
