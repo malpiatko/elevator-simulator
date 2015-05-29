e675611d326ef9026280f313d016b19a9e6d5488
ela <elzbieta.siwy@gmail.com>
2015-05-29 01:28:17 +0100

    Building as runnable.

--- src/main/java/com/locusenergy/homework/Building.java
+++ src/main/java/com/locusenergy/homework/Building.java
@@ -7,12 +7,17 @@ import java.util.List;
  * Note: nFloors means that the last floor of the building is nFloors,
  * therefore there are actually (nFloors + 1) with ground floor
  */
-public class Building implements ElevatorController{
+public class Building implements ElevatorController, Runnable {
 	
-	List<Elevator> elevators;
+	List<ElevatorImp> elevators;
 	final int minFloor = 0;
 	final int maxFloor;
 	final int nElev;
+	
+	Thread buldingThread;
+	private volatile boolean daytime;
+	
+	
 	
 	Building(int nElev, int nFloors) {
 		this.maxFloor = nFloors;
@@ -32,10 +37,46 @@ public class Building implements ElevatorController{
 	}
 	
 	private void createElevators() {
-		this.elevators = new ArrayList<Elevator>();
+		this.elevators = new ArrayList<ElevatorImp>();
 		for(int i = 0; i <= nElev; i++) {
 			elevators.add(new ElevatorImp(i+1, maxFloor));
 		}
+	}
+	
+	public void switchOnElevators() {
+		for(ElevatorImp e: elevators) {
+			e.switchOn();
+		}
+	}
+	
+	public void switchOffElevators() {
+		for(ElevatorImp e: elevators) {
+			e.switchOff();
+		}
+	}
+	
+	public void startDay() {
+		if(buldingThread == null) {
+			buldingThread = new Thread();
+		}
+		daytime = true;
+		buldingThread.start();
+	}
+	
+	public static void main(String[] args) {
+		Building building = new Building(1, 100);
+		building.startDay();
+	}
+
+	@Override
+	public void run() {
+		switchOnElevators();
+		while(daytime) {
+			
+		}
+		switchOffElevators();
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
